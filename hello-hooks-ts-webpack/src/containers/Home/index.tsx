1a83f075467bfa0a9ca89379a24a186d4362787c
tiankun <tiankun@tuya.com>
2020-08-11 22:41:38 +0800

    new: 添加表格

--- hello-hooks-ts-webpack/src/containers/Home/index.tsx
+++ hello-hooks-ts-webpack/src/containers/Home/index.tsx
@@ -27,18 +27,18 @@ import './index.less';
 // export default Home;
 
 class Home extends React.Component {
-  componentDidMount() {
-    document.onclick = this.two;
-  }
-  one(e) {
-    e.nativeEvent.stopImmediatePropagation();
-    alert(1);
-  }
-  two() {
-    alert(2);
-  }
+  // componentDidMount() {
+  //   document.onclick = this.two;
+  // }
+  // one(e) {
+  //   e.nativeEvent.stopImmediatePropagation();
+  //   alert(1);
+  // }
+  // two() {
+  //   alert(2);
+  // }
   render() {
-    return <div style={{ height: 150, width: 150, backgroundColor: '#000' }} onClick={this.one} />;
+    return <div style={{ height: 150, width: 150, backgroundColor: '#000' }} />;
   }
 }
 
