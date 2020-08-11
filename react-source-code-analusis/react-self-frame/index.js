8cf113ac60e7b1bc8865431dbd22639a7f6a75fd
tiankun <tiankun@tuya.com>
2020-08-11 22:46:02 +0800

    feat: fomat code directory

--- react-source-code-analusis/react-self-frame/index.js
+++ react-source-code-analusis/react-self-frame/index.js
@@ -1,4 +1,4 @@
-const Reac = {
+const React = {
   createElement,
 };
 
---
+++ react-source-code-analusis/react-self-frame/react/index.js
@@ -0,0 +1,13 @@
+const React = {
+  createElement,
+};
+
+const createElement = (tag, attrs, ...children) => {
+  return {
+    tags,
+    attrs,
+    childrens,
+  };
+};
+
+export default React;
